a6440d7523fd0067c48df70a1918875f0ad25ffb
Rashed-Jaman-Raj <rashed.jaaman@gmail.com>
2026-04-22 00:28:05 +0600

    navbar edit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,10 +7,15 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "../ui/navigation-menu";
+import { Switch } from "../ui/switch";
+import { Button } from "../ui/button";
+import { AiOutlineMenu } from "react-icons/ai";
+
+
 
 const Navbar = () => {
   return (
-    <header className="py-4 shadow-md ">
+    <header className="py-4 px-10 shadow-md ">
       <nav className="max-w-7xl mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8">
         {/* logo */}
         <div className="text-xl font-bold">
@@ -18,21 +23,52 @@ const Navbar = () => {
         </div>
 
         {/* desktop menu */}
-        <NavigationMenu>
+        <NavigationMenu className=" hidden mx-auto items-center  lg:flex">
           <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="/news">News</NavigationMenuLink>
+            <NavigationMenuItem className="flex items-center space-x-8">
+              <NavigationMenuLink href="/news" className=" hover:text-blue-500">News</NavigationMenuLink>
             </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Services</NavigationMenuTrigger>
+            <NavigationMenuItem className=" hover:text-blue-500">
+              <NavigationMenuTrigger className=" text-gray-700 cursor-pointer" >Services</NavigationMenuTrigger>
               <NavigationMenuContent>
-                <NavigationMenuLink>Web</NavigationMenuLink>
+                <ul className=" text-gray-600 shadow-md rounded-md px-4 py-3  ">
+                  <li>
+                <NavigationMenuLink href="/services/web">Web Development</NavigationMenuLink>
+                  </li>
+                  <li>
+                <NavigationMenuLink href="/services/apps">Mobile Apps</NavigationMenuLink>
+                  </li>
+                  <li>
+                <NavigationMenuLink href="/services/seo">Seo</NavigationMenuLink>
+                  </li>
+                </ul>
               </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="flex items-center space-x-8">
+              <NavigationMenuLink href="/about" className=" hover:text-blue-500">About</NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="flex items-center space-x-8">
+              <NavigationMenuLink href="/contact" className=" hover:text-blue-500">Contact</NavigationMenuLink>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
 
         {/* color switcher and login button */}
+        <div className=" hidden lg:flex items-center ">
+          <div className=" flex items-center space-x-4">
+            <span className="mr-2">Dark Mode</span>
+            <Switch />
+
+            <Button variant="default" className=" cursor-pointer">Login</Button>
+          </div>
+        </div>
+
+        {/* mobile hamber menu */}
+        <div className=" lg:hidden">
+          <button variant="outline" size="icon">
+            <AiOutlineMenu size={28}/>
+          </button>
+        </div>
       </nav>
     </header>
   );
