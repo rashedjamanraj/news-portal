40f7acb3548633a34affe1db5c10f88e3ee05651
Rashed-Jaman-Raj <rashed.jaaman@gmail.com>
2026-04-25 23:05:23 +0600

    latest news

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import Banner from "@/components/navbar/shared/Banner";
 import NewsCard from "@/components/navbar/shared/NewsCard";
+import { NewsItem } from "@/types/news";
+import { log } from "console";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home () {
+  
+  const res = await fetch('https://dummyjson.com/products');
+  const data = await res.json();
+
+  const news = data.products;
+
   return (
     <div className=" py-12 px-1 md:px-20">
      <Banner />
@@ -10,7 +18,12 @@ export default function Home() {
      <div className=" py-12 ">
       <h2 className=" text-2xl font-bold mb-8">Latest News</h2>
       <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        <NewsCard />
+
+        {
+          news.map((products: NewsItem) => (
+            <NewsCard key={products?.id} products={products}/>
+          ))
+        }
       </div>
      </div>
     </div>
---
+++ src/types/news.ts
@@ -0,0 +1,24 @@
+
+const news = {
+
+}
+
+
+export interface NewsItem {
+  id: number;
+  name: string;
+  ingredients: string[];
+  instructions: string[];
+  prepTimeMinutes: number;
+  cookTimeMinutes: number;
+  servings: number;
+  difficulty: "Easy" | "Medium" | "Hard";
+  cuisine: string;
+  caloriesPerServing: number;
+  tags: string[];
+  userId: number;
+  image: string;
+  rating: number;
+  reviewCount: number;
+  mealType: string[];
+}
