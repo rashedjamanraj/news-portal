9e29c2aec86112f5902b5e4b6e426a40ab37d50e
Rashed-Jaman-Raj <rashed.jaaman@gmail.com>
2026-04-25 19:12:31 +0600

    dynamic image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Banner from "@/components/navbar/shared/Banner";
+import NewsCard from "@/components/navbar/shared/NewsCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className=" py-12 px-1 md:px-20">
      <Banner />
+
+     <div className=" py-12 ">
+      <h2 className=" text-2xl font-bold mb-8">Latest News</h2>
+      <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        <NewsCard />
+      </div>
+     </div>
     </div>
   );
 }
---
+++ src/components/navbar/shared/NewsCard.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+import newsImage from "@/assests/banner.jpg"
+import { Button } from '@/components/ui/button'
+
+const NewsCard = () => {
+  return (
+    <div className='px-1 md:px-20"'>
+      <div className=' border p-4 rounded-md shadow-md'>
+      <Link href={"/"}>
+        <Image src={newsImage} alt='news image' className=' mb-5 md:h-56 rounded hover:scale-104 cursor-pointer transition-all duration-200'/>
+      </Link>
+
+      <div>
+        <h2 className=' text-xl font-semibold my-3'>Provide energy for the body. There are three kinds of carbohydrates: sugar, starch, and fibre.</h2>
+        <p className='mb-4'>Iodine is for the thyroid gland (in the neck area), which regulates growth and many body functions</p>
+        <Link href={"/"}>
+          <Button variant="default">Read More</Button>
+        </Link>
+      </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default NewsCard
