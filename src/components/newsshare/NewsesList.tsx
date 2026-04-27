d0f9928d20f3caff95aa3303813937bbb7bdcb3e
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-04-27 23:51:08 +0600

    fetch

--- src/components/newsshare/NewsesList.tsx
+++ src/components/newsshare/NewsesList.tsx
@@ -1,11 +1,15 @@
 import { Product } from '@/types/news';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const NewsesList = () => {
   const [news, setNews] = useState<Product[]>([]);
   const [search, setSearch] = useState<string>("");
   const [category, setCategory] = useState<string>("");
 
+
+  useEffect(() => {
+    
+  }, [])
   return (
     <div>
       News List
---
+++ src/lib/fetchNews.ts
@@ -0,0 +1,9 @@
+
+// const fetchNews = async (Category: string ="", search: string ="") => {
+//   try {
+//     const response = await fetch(``)
+//   } catch (error) {
+//     console.error("Error fetching news data", error);
+//     return[]
+//   }
+// }
