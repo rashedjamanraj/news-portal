5d45267531483e3c3d56d10f813cbd723b00a9dc
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-05-02 01:40:08 +0600

    category FilterC

--- src/components/newsshare/CetegoryFilter.tsx
+++ src/components/newsshare/CetegoryFilter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {
   Select,
   SelectContent,
@@ -8,27 +9,30 @@ import {
   SelectValue,
 } from "../ui/select";
 
+
 interface CategoryChangeProps {
   onCategoryChange: (category: string) => void;
 }
 
-const CetegoryFilter = ({ onCategoryChange }: CategoryChangeProps) => {
+const CategoryFilter = ({ onCategoryChange }: CategoryChangeProps) => {
   const categoryes = ["all", "tech", "health", "sports", "business"];
   return (
     <div className="flex gap-2 items-center justify-center mx-4">
       <h3 className=" font-bold text-lg shrink-0">Filter by Category: </h3>
       <Select>
-        <SelectTrigger className="w-45">
+        <SelectTrigger className="w-45 capitalize rounded-md">
           <SelectValue placeholder="Select Category" />
         </SelectTrigger>
         <SelectContent>
-          {categoryes.map((category) => (
-            <SelectItem key={category} value="category" className=" capitalize">{category}</SelectItem>
+          <SelectGroup>
+            {categoryes.map((category) => (
+            <SelectItem key={category} value={category} className=" capitalize">{category}</SelectItem>
           ))}
+          </SelectGroup>
         </SelectContent>
       </Select>
     </div>
   );
 };
 
-export default CetegoryFilter;
+export default CategoryFilter;
