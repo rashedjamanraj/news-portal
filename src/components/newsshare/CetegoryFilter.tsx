ef5e74e68ae61ac8370a64506c64396c5acd6bd7
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-05-02 18:08:22 +0600

    Category Filter

--- src/components/newsshare/CetegoryFilter.tsx
+++ src/components/newsshare/CetegoryFilter.tsx
@@ -15,11 +15,13 @@ interface CategoryChangeProps {
 }
 
 const CategoryFilter = ({ onCategoryChange }: CategoryChangeProps) => {
-  const categoryes = ["all", "tech", "health", "sports", "business"];
+  const categoryes = ["all", "beauty", "fragrances", "furniture", "groceries"];
   return (
     <div className="flex gap-2 items-center justify-center mx-4">
       <h3 className=" font-bold text-lg shrink-0">Filter by Category: </h3>
-      <Select>
+      <Select
+      onValueChange={(value) => onCategoryChange(value === "all" ? "" : value)}
+      >
         <SelectTrigger className="w-45 capitalize rounded-md">
           <SelectValue placeholder="Select Category" />
         </SelectTrigger>
